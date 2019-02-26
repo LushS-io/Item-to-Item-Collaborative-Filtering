69dcc82bb33213e27cddf6f5e1c6e1f84eaa853e
Troy Kirin <troykirin@gmail.com>
2019-02-26 12:53:45 -0800

    fix df path

--- item-item_11429784.py
+++ item-item_11429784.py
@@ -1,10 +1,9 @@
 #%% Import
 import pandas as pd
 import numpy as np
-import math as mth
-from sklearn.metrics.pairwise import cosine_similarity 
+from math import exp, sqrt, pow
+from sklearn.metrics.pairwise import cosine_similarity
 
 #%% define dataset
-df = pd.read_csv("/Users/troy/WSU_SPRING_2019/DATA_MINING/hw2/similaritymatrixdebugginginformation.csv")
+df = pd.read_csv("/Users/troy/WSU_SPRING_2019/DATA_MINING/hw2/Item to Item Collaborative Filtering/similaritymatrixdebugginginformation.csv")
 df
-#%%
