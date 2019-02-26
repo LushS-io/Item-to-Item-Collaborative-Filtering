433d87c5a44bf38d18402d50ca2ce5ce2dcdd0f4
Troy Kirin <troykirin@gmail.com>
2019-02-26 10:47:09 -0800

    import libraries and dataset

---
+++ item-item_11429784.py
@@ -0,0 +1,10 @@
+#%% Import
+import pandas as pd
+import numpy as np
+import math as mth
+from sklearn.metrics.pairwise import cosine_similarity 
+
+#%% define dataset
+dataset = pd.read_csv("/Users/troy/WSU_SPRING_2019/DATA_MINING/hw2/similaritymatrixdebugginginformation.csv")
+
+#%%
