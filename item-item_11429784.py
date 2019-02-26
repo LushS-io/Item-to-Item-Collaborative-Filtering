35aec2eb0bd9b988df13fc28b5d20563601b2b18
Troy Kirin <troykirin@gmail.com>
2019-02-26 13:46:33 -0800

    def function for cos_sim adapted for hw2

--- item-item_11429784.py
+++ item-item_11429784.py
@@ -15,3 +15,21 @@ df_test
 df_test_results = cosine_similarity(df_test)
 print(df_test_results)
 type(df_test_results)
+
+#%%
+"""Takes 2 vectors a, b and returns the cosine similarity according to the definition of the dot product"""
+def cos_sim(a, b):
+    dot_product = np.dot(a,b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return dot_product / (norm_a * norm_b)
+
+# dataset from hw 2
+user_1 = np.array([4,5,0,5,1,0]) 
+user_2 = np.array([0,3,4,3,1,2])
+user_3 = np.array([2,0,1,3,0,4])
+
+# print combination of similarities
+print(cos_sim(user_1, user_2))
+print(cos_sim(user_1,user_3))
+print(cos_sim(user_2,user_3))
