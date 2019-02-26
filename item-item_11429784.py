3e1a32764b2f4e7544dea9e544e508a391820a29
Troy Kirin <troykirin@gmail.com>
2019-02-26 13:10:08 -0800

    drop unnamed column and run cosine_similarity

--- item-item_11429784.py
+++ item-item_11429784.py
@@ -7,3 +7,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 #%% define dataset
 df = pd.read_csv("/Users/troy/WSU_SPRING_2019/DATA_MINING/hw2/Item to Item Collaborative Filtering/similaritymatrixdebugginginformation.csv")
 df
+
+#%%
+df_test = df.drop(['Unnamed: 0'],axis=1)
+#cosine_similarity(df_test)
+df_test
+df_test_results = cosine_similarity(df_test)
+print(df_test_results)
+type(df_test_results)
