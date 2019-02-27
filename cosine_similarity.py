b035cdf9b59ef06054c0141b293c55e402292566
Troy Kirin <troykirin@gmail.com>
2019-02-26 21:57:22 -0800

    adapt to a test set

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -3,9 +3,11 @@ from sklearn.metrics import pairwise_distances
 from scipy.spatial.distance import cosine
 
 A = np.array(
-[[0, 1, 0, 0, 1],
-[0, 0, 1, 1, 1],
-[1, 1, 0, 1, 0]])
+[[4,5,0,5,1,0],
+[0,3,4,3,1,2],
+[2,0,1,3,0,4]])
 
 dist_out = 1-pairwise_distances(A, metric="cosine")
 dist_out
+
+
