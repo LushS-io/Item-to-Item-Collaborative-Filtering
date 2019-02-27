841a7f1e7e0f7f8102f9542d7b29a8a38a606565
Troy Kirin <troykirin@gmail.com>
2019-02-26 23:59:29 -0800

    testing big run

--- item-item_11429784.py
+++ item-item_11429784.py
@@ -1,10 +1,12 @@
-# Homework 2 programming portion
+''' Homework 2 programming portion '''
 #%% imports
 import pandas as pd
 import numpy as np
 import math as mth
 from itertools import combinations
 from dfply import *
+from sklearn.metrics import pairwise_distances
+from scipy.spatial.distance import cosine
 
 #%% import datasets
 links = pd.read_csv("./movie-lens-data/links.csv")
@@ -13,13 +15,26 @@ ratings = pd.read_csv("./movie-lens-data/ratings.csv")
 tags = pd.read_csv("./movie-lens-data/tags.csv")
 
 #%% list attributes
-print(list(links))
 print(list(movies.columns.values)) #another way to list
 print(list(ratings))
+print(list(links))
 print(list(tags))
 
 #%% join movies and ratings
 # movies.join(other=ratings,on='movieId',lsuffix="_movies",rsuffix="_ratings")
 df = pd.merge(movies,ratings, on='movieId', how='outer')
+
+#%%
+list(df)
+
+#%%
+# group_by movie id -> then put ratings for group into a list
+# run cosine similarity on combinations based on movie_id
+new = ratings[['rating','movieId']].copy()
+new = new[:100]
+new.shape
+#%%
+dist_out = 1-pairwise_distances(new, metric="cosine")
+#%%
 
 #%%
