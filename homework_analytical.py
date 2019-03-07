a64358dedeb5fdebf042f48ebeaa027eb8e4c75e
Troy Kirin <troykirin@gmail.com>
2019-03-07 00:14:34 -0800

    The big test!

--- homework_analytical.py
+++ homework_analytical.py
@@ -90,6 +90,33 @@ print(Y.todense())
 print()
 print(Y.T.todense())
 
+#%% stacktest
+A = Y
+
+# base similarity matrix (all dot products)
+# replace this with A.dot(A.T).toarray() for sparse representation
+similarity = A.dot(A.T).toarray()
+
+
+# squared magnitude of preference vectors (number of occurrences)
+# square_mag = np.diag(similarity)
+square_mag = similarity.diagonal()
+
+# inverse squared magnitude
+inv_square_mag = 1 / square_mag
+
+# if it doesn't occur, set it's inverse magnitude to zero (instead of inf)
+inv_square_mag[np.isinf(inv_square_mag)] = 0
+
+# inverse of the magnitude
+inv_mag = np.sqrt(inv_square_mag)
+
+# cosine similarity (elementwise multiply by inverse magnitudes)
+cosine = similarity * inv_mag
+cosine = cosine.T * inv_mag
+
+print(cosine)
+
 #%% section D 
 yah = Y.todense()
 print(yah)
@@ -101,6 +128,9 @@ print(yah[0])
 print(yah[1].T)
 
 #%% 
+dot_prod = np.dot(Y,Y.T)
+print(dot_prod.todense())
+
 
 
 #%% compare
--- item-item_11429784.py
+++ item-item_11429784.py
@@ -3,38 +3,26 @@
 import pandas as pd
 import numpy as np
 import math as mth
+from sklearn.metrics.pairwise import cosine_similarity
 from itertools import combinations
 from dfply import *
+from itertools import starmap
+import scipy as scipy
+import time as time
 from scipy.sparse import csr_matrix
+import scipy.sparse as sps
+import itertools as iter
 
 #testing purposes
 from scipy.spatial.distance import cosine
 from sklearn.metrics.pairwise import cosine_similarity
 import time
 
-#%% function for cosine similarity
-"""Takes 2 vectors a, b and returns the cosine similarity according to the definition of the dot product"""
-def cos_sim(a, b):
-    dot_product = np.dot(a,b)
-    norm_a = np.linalg.norm(a)
-    norm_b = np.linalg.norm(b)
-    return dot_product / (norm_a * norm_b)
-
-def nd_combs(arr, r, axis=0):
-    arr = np.asarray(arr)
-    if axis < 0:
-        axis += arr.ndim
-    indices = np.arange(arr.shape[axis])
-    dt = np.dtype([('', np.intp)]*r)
-    indices = np.fromiter(combinations(indices, r), dt)
-    indices = indices.view(np.intp).reshape(-1, r)
-    return np.take(arr, indices, axis=axis)
-
 #%% import datasets
-links = pd.read_csv("./movie-lens-data/links.csv")
-movies = pd.read_csv("./movie-lens-data/movies.csv")
-ratings = pd.read_csv("./movie-lens-data/ratings.csv")
-tags = pd.read_csv("./movie-lens-data/tags.csv")
+links = pd.read_csv("Item to Item Collaborative Filtering/movie-lens-data/links.csv")
+movies = pd.read_csv("Item to Item Collaborative Filtering/movie-lens-data/movies.csv")
+ratings = pd.read_csv("Item to Item Collaborative Filtering/movie-lens-data/ratings.csv")
+tags = pd.read_csv("Item to Item Collaborative Filtering/movie-lens-data/tags.csv")
 
 #%% list attributes
 print(list(movies.columns.values)) #another way to list
@@ -57,69 +45,57 @@ print(type(df_mov_rat)) #check type
 
 np_mov_rat = df_mov_rat.to_numpy() #create np array
 
-#%%
-#create combos
+# %% Create CSR Sparse Matrix
+csr_mat = scipy.sparse.csr_matrix(np_mov_rat)
+
+# %% get mean of each row
+(x, y, z) = scipy.sparse.find(csr_mat)
+counts = np.bincount(x)
+sums = np.bincount(x, weights=z)
+csr_avg = sums/counts
+
+# %% normalize
+X = csr_mat
+nnz_per_row = np.diff(X.indptr)
+
+print(nnz_per_row)
+
+Y = sps.csr_matrix((X.data - np.repeat(csr_avg, nnz_per_row), X.indices, X.indptr),
+                   shape=X.shape)
+# print(Y.todense())
+# print()
+# print(Y.T.todense())
+
+#%% stacktest
 start_time = time.time()
-np_combos_mov_rat = nd_combs(np_mov_rat[0:2000,:],2,0)
-print(np_combos_mov_rat.shape)
-end_time=time.time()
+A = Y
+
+# base similarity matrix (all dot products)
+# replace this with A.dot(A.T).toarray() for sparse representation
+similarity = A.dot(A.T).toarray()
+
+
+# squared magnitude of preference vectors (number of occurrences)
+# square_mag = np.diag(similarity)
+square_mag = similarity.diagonal()
+
+# inverse squared magnitude
+inv_square_mag = 1 / square_mag
+
+# if it doesn't occur, set it's inverse magnitude to zero (instead of inf)
+inv_square_mag[np.isinf(inv_square_mag)] = 0
+
+# inverse of the magnitude
+inv_mag = np.sqrt(inv_square_mag)
+
+# cosine similarity (elementwise multiply by inverse magnitudes)
+cosine = similarity * inv_mag
+cosine = cosine.T * inv_mag
+
+end_time = time.time()
+
+print(cosine)
+
 
 Run_time = end_time - start_time
 print(Run_time)
-#%% Create matrix 
-csr_matrix(())
-
-#%% Create small subset to test on 
-df_np_small = df_mov_rat.iloc[0:10,:]
-np_small = df_np_small.to_numpy()
-# type(np_small)
-np_small.shape
-
-#%% Print to check test df 
-print(np_small)
-print(df_np_small)
-#%% run
-for row in np.nditer(np_small):
-    print(row)
-#%% to check algorithm run pairwise cosine from sklearn
-check = cosine_similarity(np_small)
-check2 = cosine_similarity(df_np_small)
-print(check)
-print('\n')
-print(check2)
-
-#%% check
-print(df_np_small)
-#%% print numpy array
-for row in np_small:
-    rating = row[0]
-    movie = row[1]
-    print(rating)
-    print(movie)
-
-#%%
-for pair in combinations(np_small[:,],2):
-    x1 = pair[0]
-    x2 = pair[1]
-    print(cos_sim(x1,x2))
-
-# group_by movie id -> then put ratings for group into a list
-# run cosine similarity on combinations based on movie_id
-#%%
-df.shape
-
-#%% check 3
-check3 = cosine_similarity(np_mov_rat)
-#%% the big guy
-the_win = np.array()
-
-for pair in combinations(np_mov_rat[:,],2):
-    x1 = pair[0]
-    x2 = pair[1]
-    np.append.(cos_sim(x1,x2))
-
-
-#%%
-
-
-#%%
